652c517eda366e4b542bd3be22f02bcba1d2ec5e
Lucian Suvaila <suvaila.lucian@agilefreaks.com>
2022-09-02 16:30:19 +0300

    Implement partially composite pattern

---
+++ structural/composite-pattern/employee.rb
@@ -0,0 +1,18 @@
+
+class Employee
+  attr_reader :subordinates
+
+  def initialize(name, department)
+    @name = name
+    @department = department
+    @subordinates = []
+  end
+
+  def add_subordinate(employee)
+    @subordinates.append(employee)
+  end
+
+  def remove_subordinate(employee)
+    @subordinates.delete(employee)
+  end
+end
