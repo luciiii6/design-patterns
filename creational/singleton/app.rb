8185effd7fe8628cbbb018b13a5786c37a47edb4
Lucian Suvaila <suvaila.lucian@agilefreaks.com>
2022-09-02 14:09:28 +0300

    Implement singleton pattern

---
+++ creational/singleton/app.rb
@@ -0,0 +1,10 @@
+
+require './singleton_example'
+
+s1 = SingletonExample.instance
+pp s1
+
+s2 = SingletonExample.instance
+pp s2
+
+pp s1 == s2
---
+++ creational/singleton/singleton_example.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class SingletonExample
+
+  class << self
+    def instance
+      @instance ||= new
+    end
+  end
+
+  private
+
+  def initialize
+    # initialize object
+  end
+end
